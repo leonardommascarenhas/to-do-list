2933aa8cee1a3519bff68affc6afcfb924bfab57
leonardo <leonardommascarenhas@gmail.com>
2022-08-19 22:16:22 -0300

    fully functioning add button

--- src/createTodo.js
+++ src/createTodo.js
@@ -2,16 +2,20 @@ import ToDo from "./toDo";
 
 export default function toDoDiv() {
   let container = document.querySelector(".toDo-container");
-  let divContainer = document.createElement("div");
-  let buttonContainer = document.createElement("div");
-  divContainer.classList = "addTaskContainer";
-  createInput(divContainer, "text", "input", "input", "Add a task", "input");
-  createButton(buttonContainer, "Add", "add-task-btn");
-  createButton(buttonContainer, "cancel", "cancel-task-btn");
-  divContainer.appendChild(buttonContainer);
-  container.appendChild(divContainer);
-  let add = document.getElementById("add-task-btn");
-  add.addEventListener("click", createTask);
+  if (container.querySelector(".addTaskContainer") == null) {
+    let divContainer = document.createElement("div");
+    let buttonContainer = document.createElement("div");
+    divContainer.classList = "addTaskContainer";
+    createInput(divContainer, "text", "input", "input", "Add a task", "input");
+    createButton(buttonContainer, "Add", "add-task-btn");
+    createButton(buttonContainer, "cancel", "cancel-task-btn");
+    divContainer.appendChild(buttonContainer);
+    container.appendChild(divContainer);
+    let add = document.getElementById("add-task-btn");
+    add.addEventListener("click", createTask);
+  } else {
+    alert("fail");
+  }
 }
 
 function createButton(append, text, id) {
@@ -39,6 +43,6 @@ function createTask() {
   let input = document.getElementById("input").value;
   container.innerHTML = input;
   ToDo.title = input;
-  divContainer.innerHTML = "";
   teste.appendChild(container);
+  divContainer.remove();
 }
